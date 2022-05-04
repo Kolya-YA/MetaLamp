bced9ec167238acaabc4efbaf353627a6927fcf1
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2022-05-04 03:14:51 +0200

    count items in dropdown js

--- src/includes/blocks/dropdown-widget/dropdown-widget.js
+++ src/includes/blocks/dropdown-widget/dropdown-widget.js
@@ -1,21 +1,13 @@
 export function dropdown() {
-    console.log("DD")
+    console.log("Dropdown")
 
-    const pluralWords = ({few, one, two}) => {
-        const titles = [one, two, few];
-      
-        return (number) => {
-          const cases = [2, 0, 1, 1, 1, 2];
-      
-          return titles[
-            number % 100 > 4 && number % 100 < 20
-              ? 2
-              : cases[number % 10 < 5 ? number % 10 : 5]
-          ];
-        };
-      };
-
-    const dropdownWigets = document.querySelectorAll('.dropdown-widget')
+    const pluralWords = number => {
+        const cases = [2, 0, 1, 1, 1, 2];
+    
+        return number % 100 > 4 && number % 100 < 20
+            ? 2
+            : cases[number % 10 < 5 ? number % 10 : 5]
+    };
 
     const ddwHandler = e => {
         if (e.target.classList.contains('dropdown-item__btn')) {
@@ -29,17 +21,33 @@ export function dropdown() {
                 if (+e.target.previousSibling.textContent > minVal)
                 e.target.previousSibling.textContent--
             }
-
+            
+            const items = []
+            const onePlural = e.currentTarget.dataset.plural && JSON.parse(e.currentTarget.dataset.plural)
+            
             e.target.parentElement.parentElement.parentElement.childNodes.forEach(item => {
                 if (item.classList.contains('dropdown-item')) {
-                    console.log(item.querySelector('.dropdown-item__qty').textContent)
-
+                    items.push([
+                        item.childNodes[1].childNodes[1].textContent,
+                        item.childNodes[0].textContent
+                    ])
                 }
             })
-        }
+
+            const placeholder = e.currentTarget.childNodes[1].firstChild.firstChild.dataset.placeholder
+            if (onePlural) {
+                const sum = items.reduce((acc, x) => acc + +x[0], 0)
+                const dropdownValue = `${sum} ${onePlural[pluralWords(sum)]}`
+                console.log(dropdownValue)
+                // e.target.parentElement.parentElement.parentElement.previousSibling.firstChild.textContent = dropdownValue
+                e.currentTarget.childNodes[1].firstChild.firstChild.textContent = sum 
+                    ? dropdownValue
+                    : placeholder
+            } else console.log((items.toString()))
+        }        
     }
 
-    dropdownWigets.forEach(w => {
+    document.querySelectorAll('.dropdown-widget').forEach(w => {
         w.addEventListener('click', ddwHandler)
     })
 }
